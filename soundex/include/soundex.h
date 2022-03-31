5439b4d485ebcbfa9d8886d57910521751fada7a
ali kalayci <kalayciali857@gmail.com>
2022-03-31 11:20:20 +0300

    not totally successful for first case

--- soundex/include/soundex.h
+++ soundex/include/soundex.h
@@ -2,48 +2,57 @@
 
 #include <algorithm>
 #include <cctype>
+#include <iostream>
 #include <string>
 #include <unordered_map>
-
-#include "string_utils.h"
 
 class Soundex
 {
   public:
   static const size_t MAX_LENGTH{4};
-  std::string clearWord(const std::string& word)
-  {
-    if (word.empty()) return pad(MAX_LENGTH);
-    std::string clearedWord = std::string(1, std::toupper(word.front()));
-    std::string tail = word.substr(1);
-    dropVowelLikeLettersFrom(tail);
-    clearedWord += tail;
-    if (clearedWord.size() < MAX_LENGTH) padWord(clearedWord);
-    return clearedWord;
-  }
+
   std::string encode(const std::string& word)
   {
-    std::string clearedWord = clearWord(word);
-    return clearedWord;
+    if (word.empty()) return pad(MAX_LENGTH);
+    std::string encodedWord = head(word);
+    char lastEncoded = encodedWord.front();
+    for (size_t i = 1; i < word.size(); ++i)
+    {
+      char digitNow = encodedDigit(word[i]);
+      if (digitNow != PADDING && digitNow != lastEncoded )
+      {
+        encodedWord += digitNow;
+        if (isMaxLength(encodedWord)) return encodedWord;
+      }
+      lastEncoded = digitNow;
+    }
+    padUntilMaxLength(encodedWord);
+    return encodedWord;
   }
 
   private:
   const char PADDING = '0';
-  std::string pad(int n) { return std::string(n, PADDING); }
-  void padWord(std::string& word) { word += pad(MAX_LENGTH - word.size()); }
-  void dropVowelLikeLettersFrom(std::string& word)
+  std::string head(const std::string& word) const
   {
-    auto vowelLike = std::remove_if(word.begin(), word.end(), isVowel);
-    word.erase(vowelLike, word.end());
+    return std::string(1, std::toupper(word.front()));
   }
-  std::string encodedDigit(char letter) const
+  std::string pad(int n) const { return std::string(n, PADDING); }
+  bool isMaxLength(const std::string& word) const
   {
-    const std::unordered_map<char, std::string> encodings{
-        {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"}, {'c', "2"}, {'g', "2"},
-        {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
-        {'d', "3"}, {'t', "3"}, {'l', "4"}, {'m', "5"}, {'n', "5"}, {'r', "6"}};
-    auto it = encodings.find(letter);
-    return it == encodings.end() ? "" : it->second;
+    return (word.size() == MAX_LENGTH);
+  }
+  void padUntilMaxLength(std::string& word)
+  {
+    word += pad(MAX_LENGTH - word.size());
+  }
+  char encodedDigit(char letter) const
+  {
+    const std::unordered_map<char, char> encodings{
+        {'b', '1'}, {'f', '1'}, {'p', '1'}, {'v', '1'}, {'c', '2'}, {'g', '2'},
+        {'j', '2'}, {'k', '2'}, {'q', '2'}, {'s', '2'}, {'x', '2'}, {'z', '2'},
+        {'d', '3'}, {'t', '3'}, {'l', '4'}, {'m', '5'}, {'n', '5'}, {'r', '6'}};
+    auto it = encodings.find(std::tolower(letter));
+    return it == encodings.end() ? PADDING : it->second;
   }
 };
 
--- soundex/soundex_test.cpp
+++ soundex/soundex_test.cpp
@@ -10,25 +10,46 @@ class SoundexEncoding : public testing::Test
 };
 
 TEST_F(SoundexEncoding, FillWithPaddingIfEmpty) {
-  ASSERT_EQ(soundex.clearWord(""), "0000");
+  ASSERT_EQ(soundex.encode(""), "0000");
 }
 
 TEST_F(SoundexEncoding, KeepOneLetterWord) {
   ASSERT_EQ(soundex.encode("A"), "A000");
 }
 
-TEST_F(SoundexEncoding, FillWithPaddingIfLessThanMaxLength) {
-  ASSERT_EQ(soundex.clearWord("Ab"), "Ab00");
+TEST_F(SoundexEncoding, EncodeConsonantsWithDigits) {
+  ASSERT_EQ(soundex.encode("Dx"), "D200");
 }
 
-TEST_F(SoundexEncoding, DropVowelLikeLettersCaseInsensitive) {
-  ASSERT_EQ(soundex.clearWord("AceahwyAeIou"), "Ac00");
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+  ASSERT_EQ(soundex.encode("BaAeEiIoOuUhHyYcdl"), "B234");
 }
+
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+  ASSERT_EQ(soundex.encode("A#"), "A000");
+}
+
 
 TEST_F(SoundexEncoding, KeepHeadUppercase) {
-  ASSERT_EQ(soundex.clearWord("sheahw"), "S000");
+  ASSERT_EQ(soundex.encode("sheahw"), "S000");
 }
 
-TEST_F(SoundexEncoding, DISABLED_ReplaceConsonantsWithDigits) {
-  ASSERT_EQ(soundex.encode("Dx"), "D200");
+TEST_F(SoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
+  ASSERT_EQ(soundex.encode("BCDL"), soundex.encode("Bcdl"));
+}
+
+TEST_F(SoundexEncoding, TreatAsOneDigitIfTwoSameConsonant) {
+  EXPECT_EQ(soundex.encode("Ab"), soundex.encode("Af"));
+  EXPECT_EQ(soundex.encode("Ac"), soundex.encode("Ag"));
+  EXPECT_EQ(soundex.encode("Ad"), soundex.encode("At"));
+  ASSERT_EQ(soundex.encode("Abfcgdt"), "A123");
+}
+
+TEST_F(SoundexEncoding, StopWhenMaxLengthAchieved) {
+  ASSERT_EQ(soundex.encode("baxxcdl"), "B234");
+}
+
+TEST_F(SoundexEncoding, DontTreatAsOneDigitIfSeparatedByVowelLike) {
+  ASSERT_EQ(soundex.encode("Jbob"), "J110");
 }
